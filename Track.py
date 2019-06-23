c9d9a06fec2523a5abcf69fa6bddfafd509745cb
ldonohoe <donohl@rpi.edu>
2019-06-22 22:06:26 -0400

    Removed old unused files

--- Track.py
+++
@@ -1,7 +0,0 @@
-class Track(object):
-	def __init_(self, screen, preset=0):
-		# Need set two sets of points, 
-		self.innerPoints
-		self.outerPoints
-		self.screenWidth = screen.get_width()
-		self.screenheight = screen.get_height()
